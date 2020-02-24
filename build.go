e9e336002712efa6a348c6b84946dca80adbec98
A Gardner <3100188+actgardner@users.noreply.github.com>
2020-02-24 17:13:43 -0500

    Handle volumes correctly
    Signed-off-by: A Gardner <3100188+actgardner@users.noreply.github.com>

--- build.go
+++ build.go
@@ -543,8 +543,8 @@ func buildPlatformVolumes(volumes []string) ([]string, error) {
 		}
 
 		// Use path.Join instead of filepath.Join because we assume the container OS is linux but the host may be windows
-		volume.Destination = path.Join("/platform", volume.Destination)
-		platformVolumes[i] = fmt.Sprintf("%v:%v:ro", volume.Source, volume.Destination)
+		dest := path.Join("/platform", volume.Destination)
+		platformVolumes[i] = fmt.Sprintf("%v:%v:ro", volume.Spec.Source, dest)
 	}
 	return platformVolumes, nil
 }
