e5ff848f1fbe07f98550d09cf8a199a24e62a8be
David Freilich <dfreilich@vmware.com>
2020-05-19 17:31:42 -0400

    Use test scoped version
    Signed-off-by: David Freilich <dfreilich@vmware.com>

--- internal/commands/version_test.go
+++ internal/commands/version_test.go
@@ -8,7 +8,6 @@ import (
 	"github.com/sclevine/spec/report"
 	"github.com/spf13/cobra"
 
-	"github.com/buildpacks/pack/cmd"
 	"github.com/buildpacks/pack/internal/commands"
 	"github.com/buildpacks/pack/internal/logging"
 	h "github.com/buildpacks/pack/testhelpers"
@@ -20,19 +19,20 @@ func TestVersion(t *testing.T) {
 
 func testVersionCommand(t *testing.T, when spec.G, it spec.S) {
 	var (
-		command *cobra.Command
-		outBuf  bytes.Buffer
+		command     *cobra.Command
+		outBuf      bytes.Buffer
+		testVersion = "1.3.4"
 	)
 
 	it.Before(func() {
-		command = commands.Version(logging.NewLogWithWriters(&outBuf, &outBuf), cmd.Version)
+		command = commands.Version(logging.NewLogWithWriters(&outBuf, &outBuf), testVersion)
 	})
 
 	when("#Version", func() {
 		it("returns version", func() {
 			command.SetArgs([]string{})
 			h.AssertNil(t, command.Execute())
-			h.AssertEq(t, outBuf.String(), cmd.Version+"\n")
+			h.AssertEq(t, outBuf.String(), testVersion+"\n")
 		})
 	})
 }
