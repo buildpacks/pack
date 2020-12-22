014c035f7c5543d074e2093b5a0c1eda5098700e
Clivern <hello@clivern.com>
2020-12-22 09:17:50 +0100

    test strings with color enabled, style pkg
    Signed-off-by: Clivern <hello@clivern.com>

--- internal/style/style_test.go
+++ internal/style/style_test.go
@@ -18,22 +18,71 @@ func TestStyle(t *testing.T) {
 }
 
 func testStyle(t *testing.T, when spec.G, it spec.S) {
-	when("#Style", func() {
-		it("Symbol function should return the expected value", func() {
+	when("#Symbol", func() {
+		it("It should return the expected value", func() {
 			h.AssertEq(t, style.Symbol("Symbol"), "'Symbol'")
 		})
 
-		it("Symbol function should return an empty string", func() {
+		it("It should return an empty string", func() {
 			h.AssertEq(t, style.Symbol(""), "''")
 		})
 
-		it("Map function should return a string with all key value pairs", func() {
+		it("It should return the expected value while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.Symbol("Symbol"), "\x1b[94mSymbol\x1b[0m")
+		})
+
+		it("It should return an empty string while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.Symbol(""), "\x1b[94m\x1b[0m")
+		})
+	})
+
+	when("#SymbolF", func() {
+		it("It should return the expected value", func() {
+			h.AssertEq(t, style.SymbolF("values %s %d", "hello", 1), "'values hello 1'")
+		})
+
+		it("It should return an empty string", func() {
+			h.AssertEq(t, style.SymbolF(""), "''")
+		})
+
+		it("It should return the expected value while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.SymbolF("values %s %d", "hello", 1), "\x1b[94mvalues hello 1\x1b[0m")
+		})
+
+		it("It should return an empty string while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.SymbolF(""), "\x1b[94m\x1b[0m")
+		})
+	})
+
+	when("#Map", func() {
+		it("It should return a string with all key value pairs", func() {
 			h.AssertEq(t, style.Map(map[string]string{"FOO": "foo", "BAR": "bar"}, "", " "), "'FOO=foo BAR=bar'")
 			h.AssertEq(t, style.Map(map[string]string{"FOO": "foo", "BAR": "bar"}, "  ", "\n"), "'FOO=foo\n  BAR=bar'")
 		})
 
-		it("Map function should return an empty string", func() {
+		it("It should return a string with all key value pairs while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.Map(map[string]string{"FOO": "foo", "BAR": "bar"}, "", " "), "\x1b[94mFOO=foo BAR=bar\x1b[0m")
+			h.AssertEq(t, style.Map(map[string]string{"VAR1": "var1", "VAR2": "var2"}, "  ", "\n"), "\x1b[94mVAR1=var1\n  VAR2=var2\x1b[0m")
+		})
+
+		it("It should return an empty string", func() {
 			h.AssertEq(t, style.Map(map[string]string{}, "", " "), "''")
+		})
+
+		it("It should return an empty string while color enabled", func() {
+			color.Disable(false)
+			defer color.Disable(true)
+			h.AssertEq(t, style.Map(map[string]string{}, "", " "), "\x1b[94m\x1b[0m")
 		})
 	})
 }
