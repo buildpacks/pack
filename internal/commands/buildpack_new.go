d79bbd2ecdec8c5dae908e7e236171ded4eb2197
Joe Kutner <jpkutner@gmail.com>
2021-01-23 09:20:20 -0600

    Update internal/commands/buildpack_new.go
    Signed-off-by: Joe Kutner <jpkutner@gmail.com>
    
    Co-authored-by: David Freilich <dfreilich@vmware.com>

--- internal/commands/buildpack_new.go
+++ internal/commands/buildpack_new.go
@@ -33,7 +33,7 @@ func BuildpackNew(logger logging.Logger, client BuildpackCreator) *cobra.Command
 		Short:   "Creates basic scaffolding of a buildpack.",
 		Args:    cobra.ExactValidArgs(1),
 		Example: "pack buildpack new sample/my-buildpack",
-		Long:    "buildpack create generates the basic scaffolding of a buildpack repository.",
+		Long:    "buildpack new generates the basic scaffolding of a buildpack repository. It creates a new directory `name` in the current directory (or at `path`, if passed as a flag), and initializes a buildpack.toml, and two executable bash scripts, `bin/detect` and `bin/build`. ",
 		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
 			id := args[0]
 			idParts := strings.Split(id, "/")
