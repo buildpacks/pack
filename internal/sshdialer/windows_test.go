ddf2b36b172f8ac2683443b52dff6ce7a4ad9c47
Matej Vasek <mvasek@redhat.com>
2022-07-26 20:42:31 +0200

    WIP do not fixup mod
    Signed-off-by: Matej Vasek <mvasek@redhat.com>

--- internal/sshdialer/windows_test.go
+++ internal/sshdialer/windows_test.go
@@ -6,28 +6,12 @@ package sshdialer_test
 import (
 	"errors"
 	"net"
-	"os/user"
 	"strings"
 
-	"github.com/hectane/go-acl"
 	"gopkg.in/natefinch/npipe.v2"
 )
 
 func fixupPrivateKeyMod(path string) {
-	usr, err := user.Current()
-	if err != nil {
-		panic(err)
-	}
-	mode := uint32(0400)
-	err = acl.Apply(path,
-		true,
-		false,
-		acl.GrantName(((mode&0700)<<23)|((mode&0200)<<9), usr.Name))
-
-	// See https://github.com/hectane/go-acl/issues/1
-	if err != nil && err.Error() != "The operation completed successfully." {
-		panic(err)
-	}
 }
 
 func listen(addr string) (net.Listener, error) {
