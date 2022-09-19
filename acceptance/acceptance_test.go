fa076da14207464561b03e6424003a7362742089
Natalie Arellano <narellano@vmware.com>
2022-09-19 17:10:27 -0400

    Test: Windows layer diff IDs seem non-deterministic
    Signed-off-by: Natalie Arellano <narellano@vmware.com>

--- acceptance/acceptance_test.go
+++ acceptance/acceptance_test.go
@@ -745,8 +745,10 @@ func testAcceptance(
 					assertOutput.ReportsSuccesfulRunImageMirrorsAdd("pack-test/run", "some-registry.com/pack-test/run1")
 				})
 				when("builder has duplicate buildpacks", func() {
-					it("buildpack layers have no duplication", func() {
+					it.Focus("buildpack layers have no duplication", func() {
 						assertImage.DoesNotHaveDuplicateLayers(builderName)
+						layersLabel := `{"noop.buildpack":{"noop.buildpack.later-version":{"api":"0.2","stacks":[{"id":"pack.test.stack"}],"layerDiffID":"sha256:ee2739e498d992e171ddb42c092a4b97f2954186caa3701c50de91af8bed1b3b","homepage":"http://geocities.com/cool-bp","name":"NOOP Buildpack"},"noop.buildpack.version":{"api":"0.2","stacks":[{"id":"pack.test.stack"}],"layerDiffID":"sha256:9db1307b50ab44fdc2b78fc912fcfc61ddee62f9560c3a0d4fb7bd55dfa8f568","name":"NOOP Buildpack"}},"read/env":{"read-env-version":{"api":"0.2","stacks":[{"id":"pack.test.stack"}],"layerDiffID":"sha256:dd1e0efcbf3f08b014ef6eff9cfe7a9eac1cf20bd9b6a71a946f0a74575aa56f","name":"Read Env Buildpack"}},"simple/layers":{"simple-layers-version":{"api":"0.2","stacks":[{"id":"pack.test.stack"}],"layerDiffID":"sha256:a9a7b0857edb69ea8e3dde78b68796631886f7996d271792757e62fb1be7213e","name":"Simple Layers Buildpack"}},"simple/nested-level-1":{"nested-l1-version":{"api":"0.2","order":[{"group":[{"id":"simple/nested-level-2","version":"nested-l2-version"}]}],"layerDiffID":"sha256:95f6b555ebdc9ef964dd9010a42bd894ca98cbf9341121ba6042788d6c968782","name":"Nested Level One Buildpack"}},"simple/nested-level-2":{"nested-l2-version":{"api":"0.2","order":[{"group":[{"id":"simple/layers","version":"simple-layers-version"}]}],"layerDiffID":"sha256:96e21ba8fc7ef252b273a5cf1ba41be590df25c6e090b2c577d244230066afd9","name":"Nested Level Two Buildpack"}}}`
+						assertImage.HasLabelWithData(builderName, "io.buildpacks.buildpack.layers", layersLabel)
 					})
 				})
 			})
