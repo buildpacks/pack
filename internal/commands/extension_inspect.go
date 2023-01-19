87938981545514ac17727762f3a1c69ad64496e4
Darshan Kumar <itsdarshankumar@gmail.com>
2023-01-19 09:52:18 +0530

    add pack extension --help
    Signed-off-by: Darshan Kumar <itsdarshankumar@gmail.com>

---
+++ internal/commands/extension_inspect.go
@@ -0,0 +1,41 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/buildpacks/pack/internal/config"
+	"github.com/buildpacks/pack/pkg/logging"
+)
+
+type ExtensionInspectFlags struct {
+	Depth    int
+	Registry string
+	Verbose  bool
+}
+
+func ExtensionInspect(logger logging.Logger, cfg config.Config, client PackClient) *cobra.Command {
+	var flags ExtensionInspectFlags
+	cmd := &cobra.Command{
+		Use:     "inspect <extension-name>",
+		Args:    cobra.ExactArgs(1),
+		Short:   "Show information about an extension",
+		Example: "pack extension inspect <example-extension>",
+		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
+			extensionName := args[0]
+			registry := flags.Registry
+			if registry == "" {
+				//fix registry for extension
+			}
+
+			return extensionInspect(logger, extensionName, registry, flags, cfg, client)
+		}),
+	}
+    //flags will be added here
+	AddHelpFlag(cmd, "inspect")
+	return cmd
+}
+
+func extensionInspect(logger logging.Logger, extensionName, registry string, flags ExtensionInspectFlags, cfg config.Config, client PackClient) error {
+//logic to inspect extension
+return nil
+}
---
+++ internal/commands/extension_package.go
@@ -0,0 +1,39 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/buildpacks/pack/internal/config"
+	"github.com/buildpacks/pack/pkg/logging"
+)
+
+// ExtensionPackageFlags define flags provided to the ExtensionPackage command
+type ExtensionPackageFlags struct {
+	PackageTomlPath   string
+	Format            string
+	Publish           bool
+	Policy            string
+	ExtensionRegistry string
+	Path              string
+}
+
+//Packager and PackageConfigReader to be added here and argument also to be added in the function
+
+// ExtensionPackage packages (a) extension(s) into OCI format, based on a package config
+func ExtensionPackage(logger logging.Logger, cfg config.Config) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "package <name> --config <config-path>",
+		Short:   "Package an extension in OCI format.",
+		Args:    cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
+            //logic will be added here
+			return nil
+		}),
+	}
+
+	//flags will be added here
+
+	AddHelpFlag(cmd, "package")
+	return cmd
+}
+
