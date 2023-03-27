08ec40676d0503ed95c3317d35d5443d879b9485
Husni Faiz <ahamedhusni73@gmail.com>
2023-03-27 19:28:12 +0530

    cmd: manifest: refactor code
    Use source/sinks instead of using primitives directly
    
    Signed-off-by: Husni Faiz <ahamedhusni73@gmail.com>

--- internal/commands/manifest_create.go
+++ internal/commands/manifest_create.go
@@ -1,23 +1,23 @@
 package commands
 
 import (
-	"encoding/json"
+	v2 "github.com/buildpacks/imgutil/remote"
 
-	"github.com/buildpacks/imgutil"
-	"github.com/buildpacks/imgutil/remote"
+	"github.com/buildpacks/imgutil/layout"
 	"github.com/buildpacks/pack/pkg/logging"
 	"github.com/google/go-containerregistry/pkg/authn"
-	"github.com/google/go-containerregistry/pkg/crane"
+	v1 "github.com/google/go-containerregistry/pkg/v1"
 
-	// v1 "github.com/google/go-containerregistry/pkg/v1"
-	"github.com/opencontainers/go-digest"
-	v1 "github.com/opencontainers/image-spec/specs-go/v1"
 	"github.com/spf13/cobra"
+
+	"github.com/google/go-containerregistry/pkg/name"
+	"github.com/google/go-containerregistry/pkg/v1/empty"
+	"github.com/google/go-containerregistry/pkg/v1/mutate"
+	"github.com/google/go-containerregistry/pkg/v1/remote"
+	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
-// BuildpackNew generates the scaffolding of a buildpack
 func ManifestCreate(logger logging.Logger) *cobra.Command {
-	// var flags BuildpackNewFlags
 	cmd := &cobra.Command{
 		Use:   "create <id>",
 		Short: "Creates a manifest list",
@@ -26,62 +26,60 @@ func ManifestCreate(logger logging.Logger) *cobra.Command {
 				 paketobuildpacks/builder:full-linux-arm`,
 		Long: "manifest create generates a manifest list for a multi-arch image",
 		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
-			var manifests []v1.Descriptor
-			desc := v1.Descriptor{}
+
+			var index v1.ImageIndex
+
+			// initialize and set the media type
+			index = empty.Index
+			index = mutate.IndexMediaType(index, types.DockerManifestList)
+
+			var adds []mutate.IndexAddendum
 
 			for _, j := range args[1:] {
-				digestVal, err := crane.Digest(j)
+
+				ref, err := name.ParseReference(j)
 				if err != nil {
-					return err
+					panic(err)
 				}
 
-				manifestVal, err := crane.Manifest(j)
+				img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
 				if err != nil {
-					return err
+					panic(err)
 				}
 
-				manifest := v1.Manifest{}
-				json.Unmarshal(manifestVal, &manifest)
+				desc, err := remote.Get(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
+				if err != nil {
+					panic(err)
+				}
 
-				img, err := remote.NewImage(
+				// ##################################################
+
+				img2, err := v2.NewImage(
 					"registry-1.docker.io",
 					authn.DefaultKeychain,
-					remote.FromBaseImage(j),
-					remote.WithDefaultPlatform(imgutil.Platform{
-						OS:           "linux",
-						Architecture: "amd64",
-					}),
+					v2.FromBaseImage(j),
 				)
 
-				// fmt.Println(manifest)
-				os, _ := img.OS()
-				arch, _ := img.Architecture()
+				os, _ := img2.OS()
+				arch, _ := img2.Architecture()
 
 				platform := v1.Platform{}
 				platform.Architecture = arch
 				platform.OS = os
 
-				desc.Size, _ = img.ManifestSize()
-				desc.Platform = &platform
-				desc.Digest = digest.Digest(digestVal)
-				desc.MediaType = manifest.MediaType
+				desc.Descriptor.Platform = &platform
+				logger.Infof(desc.Descriptor.Platform.OS + "-" + desc.Descriptor.Platform.Architecture)
 
-				manifests = append(manifests, desc)
+				// ##################################################
+
+				adds = append(adds, mutate.IndexAddendum{Add: img, Descriptor: desc.Descriptor})
 
 			}
 
-			index := v1.Index{}
-			index.SchemaVersion = 2
-			index.MediaType = "application/vnd.oci.image.index.v1+json"
-			index.Manifests = manifests
+			index = mutate.AppendManifests(index, adds...)
 
-			// for _, j := range manifests {
-			// 	de, _ := json.Marshal(j)
-			// 	logger.Infof(string(de))
-			// }
-
-			de, _ := json.Marshal(index)
-			logger.Infof(string(de))
+			// write the index on disk, for example
+			layout.Write("out/", index)
 
 			return nil
 		}),
