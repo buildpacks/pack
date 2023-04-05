3fc79cc01f17ac8eb1609faa7228a46777028821
Husni Faiz <ahamedhusni73@gmail.com>
2023-04-05 15:02:11 +0530

    cmd: manifest: use imgutil
    Signed-off-by: Husni Faiz <ahamedhusni73@gmail.com>

--- internal/commands/manifest_create.go
+++ internal/commands/manifest_create.go
@@ -1,22 +1,10 @@
 package commands
 
 import (
-	"fmt"
-
-	"github.com/pkg/errors"
-
-	"github.com/buildpacks/imgutil/layout"
+	"github.com/buildpacks/imgutil/remote"
 	"github.com/buildpacks/pack/pkg/logging"
-	"github.com/google/go-containerregistry/pkg/authn"
-	v1 "github.com/google/go-containerregistry/pkg/v1"
 
 	"github.com/spf13/cobra"
-
-	"github.com/google/go-containerregistry/pkg/name"
-	"github.com/google/go-containerregistry/pkg/v1/empty"
-	"github.com/google/go-containerregistry/pkg/v1/mutate"
-	"github.com/google/go-containerregistry/pkg/v1/remote"
-	"github.com/google/go-containerregistry/pkg/v1/types"
 )
 
 func ManifestCreate(logger logging.Logger) *cobra.Command {
@@ -29,59 +17,15 @@ func ManifestCreate(logger logging.Logger) *cobra.Command {
 		Long: "manifest create generates a manifest list for a multi-arch image",
 		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
 
-			var index v1.ImageIndex
+			idx, _ := remote.NewIndex(args[0]) // This will return an empty index
 
-			// initialize and set the media type
-			index = empty.Index
-			index = mutate.IndexMediaType(index, types.DockerManifestList)
-
-			var adds []mutate.IndexAddendum
-
+			// Add every manifest to image index
 			for _, j := range args[1:] {
-
-				ref, err := name.ParseReference(j)
-				if err != nil {
-					panic(err)
-				}
-
-				img, err := remote.Image(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
-				if err != nil {
-					panic(err)
-				}
-
-				desc, err := remote.Get(ref, remote.WithAuthFromKeychain(authn.DefaultKeychain))
-				if err != nil {
-					panic(err)
-				}
-
-				cfg, err := img.ConfigFile()
-
-				if err != nil {
-					return errors.Wrapf(err, "getting config file for image %q", j)
-				}
-				if cfg == nil {
-					return fmt.Errorf("missing config for image %q", j)
-				}
-				if cfg.OS == "" {
-					return fmt.Errorf("missing OS for image %q", j)
-				}
-
-				// desc.Descriptor.Platform
-
-				platform := v1.Platform{}
-				platform.Architecture = cfg.Architecture
-				platform.OS = cfg.OS
-
-				desc.Descriptor.Platform = &platform
-
-				adds = append(adds, mutate.IndexAddendum{Add: img, Descriptor: desc.Descriptor})
-
+				idx.Add(j)
 			}
 
-			index = mutate.AppendManifests(index, adds...)
-
-			// write the index on disk, for example
-			layout.Write("out/", index)
+			// Store layout in local storage
+			idx.Save("out/index")
 
 			return nil
 		}),
