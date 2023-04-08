623dcf51e3f7eff3e67b996d401dc1f2de4ba4de
Aidan Delaney <adelaney21@bloomberg.net>
2023-04-08 17:18:46 +0100

    Bump to latest lifecycle release
    Default to 0.16.1
    
    Signed-off-by: Aidan Delaney <adelaney21@bloomberg.net>

--- internal/builder/lifecycle.go
+++ internal/builder/lifecycle.go
@@ -14,7 +14,7 @@ import (
 
 // A snapshot of the latest tested lifecycle version values
 const (
-	DefaultLifecycleVersion    = "0.16.0"
+	DefaultLifecycleVersion    = "0.16.1"
 	DefaultBuildpackAPIVersion = "0.2"
 )
 
