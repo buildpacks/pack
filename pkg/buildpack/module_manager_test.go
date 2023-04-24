3ccd9dd79ebccafcb68e7c0fd16820dc057db870
Juan Bustamante <jbustamante@vmware.com>
2023-04-24 18:42:06 -0500

    adding flatten module test
    Signed-off-by: Juan Bustamante <jbustamante@vmware.com>

--- pkg/buildpack/module_manager_test.go
+++ pkg/buildpack/module_manager_test.go
@@ -21,38 +21,165 @@ func TestModuleManager(t *testing.T) {
 }
 
 func testModuleManager(t *testing.T, when spec.G, it spec.S) {
+	/* compositeBP1
+	 *    /    \
+	 *   bp1   compositeBP2
+	 *           /   |    \
+	 *		  bp21 bp22 compositeBP3
+	 *						 |
+	 *						bp31
+	 */
 	var (
 		moduleManager *buildpack.ModuleManager
-		bp1v1         buildpack.BuildModule
+		compositeBP1  buildpack.BuildModule
+		bp1           buildpack.BuildModule
+		compositeBP2  buildpack.BuildModule
+		bp21          buildpack.BuildModule
+		bp22          buildpack.BuildModule
+		compositeBP3  buildpack.BuildModule
+		bp31          buildpack.BuildModule
 		err           error
 	)
 
 	it.Before(func() {
-		bp1v1, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+		bp1, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
 			WithAPI: api.MustParse("0.2"),
 			WithInfo: dist.ModuleInfo{
 				ID:      "buildpack-1-id",
-				Version: "buildpack-1-version-1",
+				Version: "buildpack-1-version",
 			},
-			WithStacks: []dist.Stack{{
-				ID:     "some.stack.id",
-				Mixins: []string{"mixinX", "mixinY"},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		bp21, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "buildpack-21-id",
+				Version: "buildpack-21-version",
+			},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		bp22, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "buildpack-22-id",
+				Version: "buildpack-22-version",
+			},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		bp31, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "buildpack-31-id",
+				Version: "buildpack-31-version",
+			},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		compositeBP3, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "composite-buildpack-3-id",
+				Version: "composite-buildpack-3-version",
+			},
+			WithOrder: []dist.OrderEntry{{
+				Group: []dist.ModuleRef{
+					{
+						ModuleInfo: bp31.Descriptor().Info(),
+						Optional:   true,
+					},
+				},
+			}},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		compositeBP2, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "composite-buildpack-2-id",
+				Version: "composite-buildpack-2-version",
+			},
+			WithOrder: []dist.OrderEntry{{
+				Group: []dist.ModuleRef{
+					{
+						ModuleInfo: bp21.Descriptor().Info(),
+						Optional:   true,
+					},
+					{
+						ModuleInfo: bp22.Descriptor().Info(),
+						Optional:   false,
+					},
+					{
+						ModuleInfo: compositeBP3.Descriptor().Info(),
+						Optional:   false,
+					},
+				},
+			}},
+		}, 0644)
+		h.AssertNil(t, err)
+
+		compositeBP1, err = ifakes.NewFakeBuildpack(dist.BuildpackDescriptor{
+			WithAPI: api.MustParse("0.2"),
+			WithInfo: dist.ModuleInfo{
+				ID:      "composite-buildpack-1-id",
+				Version: "composite-buildpack-1-version",
+			},
+			WithOrder: []dist.OrderEntry{{
+				Group: []dist.ModuleRef{
+					{
+						ModuleInfo: bp1.Descriptor().Info(),
+						Optional:   false,
+					},
+					{
+						ModuleInfo: compositeBP2.Descriptor().Info(),
+						Optional:   false,
+					},
+				},
 			}},
 		}, 0644)
 		h.AssertNil(t, err)
 	})
 
 	when("manager is configured in flatten mode", func() {
-		when("#AddModules", func() {
+		when("flatten all", func() {
+			it.Before(func() {
+				moduleManager = buildpack.NewModuleManager(true, buildpack.FlattenMaxDepth)
+				moduleManager.AddModules(compositeBP1, []buildpack.BuildModule{bp1, compositeBP2}...)
+			})
 
+			when("#GetFlattenModules", func() {
+				it("returns one big module", func() {
+					modules := moduleManager.GetFlattenModules()
+					h.AssertEq(t, len(modules), 1)
+				})
+			})
+
+			when("#IsFlatten", func() {
+				it("returns true", func() {
+					// check a composite leaf module
+					h.AssertFalse(t, moduleManager.IsFlatten(bp31))
+				})
+			})
 		})
 
-		when("#GetFlattenModules", func() {
+		when("flatten with max depth", func() {
+			it.Before(func() {
+				moduleManager = buildpack.NewModuleManager(true, 2)
+			})
 
-		})
+			when("#AddModules", func() {
 
-		when("#IsFlatten", func() {
+			})
 
+			when("#GetFlattenModules", func() {
+
+			})
+
+			when("#IsFlatten", func() {
+
+			})
 		})
 	})
 
@@ -69,11 +196,11 @@ func testModuleManager(t *testing.T, when spec.G, it spec.S) {
 
 			when("modules are added", func() {
 				it.Before(func() {
-					moduleManager.AddModules(bp1v1)
+					moduleManager.AddModules(compositeBP1, []buildpack.BuildModule{bp1, compositeBP2}...)
 				})
 				it("returns the modules added", func() {
 					modules := moduleManager.Modules()
-					h.AssertEq(t, len(modules), 1)
+					h.AssertEq(t, len(modules), 3)
 				})
 			})
 		})
@@ -86,7 +213,7 @@ func testModuleManager(t *testing.T, when spec.G, it spec.S) {
 
 			when("modules are added", func() {
 				it.Before(func() {
-					moduleManager.AddModules(bp1v1)
+					moduleManager.AddModules(compositeBP1, []buildpack.BuildModule{bp1, compositeBP2}...)
 				})
 				it("returns nil", func() {
 					modules := moduleManager.GetFlattenModules()
@@ -97,15 +224,15 @@ func testModuleManager(t *testing.T, when spec.G, it spec.S) {
 
 		when("#IsFlatten", func() {
 			it("returns false when no modules are added", func() {
-				h.AssertFalse(t, moduleManager.IsFlatten(bp1v1))
+				h.AssertFalse(t, moduleManager.IsFlatten(bp1))
 			})
 
 			when("modules are added", func() {
 				it.Before(func() {
-					moduleManager.AddModules(bp1v1)
+					moduleManager.AddModules(compositeBP1, []buildpack.BuildModule{bp1, compositeBP2}...)
 				})
 				it("returns false", func() {
-					h.AssertFalse(t, moduleManager.IsFlatten(bp1v1))
+					h.AssertFalse(t, moduleManager.IsFlatten(compositeBP2))
 				})
 			})
 		})
