8460b977be8ae4b49d24a095a38a90cf1daf4fbe
Husni Faiz <ahamedhusni73@gmail.com>
2023-05-02 21:51:35 +0530

    manifest: register subcommands
    Signed-off-by: Husni Faiz <ahamedhusni73@gmail.com>

--- internal/commands/manifest.go
+++ internal/commands/manifest.go
@@ -14,6 +14,10 @@ func NewManifestCommand(logger logging.Logger, client PackClient) *cobra.Command
 	}
 
 	cmd.AddCommand(ManifestCreate(logger, client))
+	cmd.AddCommand(ManifestAnnotate(logger, client))
+	cmd.AddCommand(ManifestAdd(logger, client))
+	cmd.AddCommand(ManifestPush(logger, client))
+	cmd.AddCommand(ManifestRemove(logger, client))
 
 	AddHelpFlag(cmd, "manifest")
 	return cmd
