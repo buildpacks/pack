1338d4dbd2a00f07faf1483ee33c7dd766198f37
github-action-benchmark <github@users.noreply.github.com>
2023-05-04 12:36:04 +0000

    add Go Benchmark (go) benchmark result for 639d2a843831d83317093f36273928ae60ffefb2

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1683200817393,
+  "lastUpdate": 1683203764789,
   "repoUrl": "https://github.com/buildpacks/pack",
   "entries": {
     "Go Benchmark": [
@@ -40,6 +40,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkBuild/with_Addtional_Buildpack",
             "value": 27957377533,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "freilich.david@gmail.com",
+            "name": "David Freilich",
+            "username": "dfreilich"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "639d2a843831d83317093f36273928ae60ffefb2",
+          "message": "Merge pull request #1741 from inspirit941/fix-1709\n\nExtract internal/cache package to public\r\nSigned-off-by: David Freilich <freilich.david@gmail.com>",
+          "timestamp": "2023-05-04T15:33:50+03:00",
+          "tree_id": "28db4d94a0cb91165d3bfe0b087aa58fcb5ac61e",
+          "url": "https://github.com/buildpacks/pack/commit/639d2a843831d83317093f36273928ae60ffefb2"
+        },
+        "date": 1683203763664,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 8745871142,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 2673230163,
+            "unit": "ns/op",
+            "extra": "1 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Addtional_Buildpack",
+            "value": 41734972476,
             "unit": "ns/op",
             "extra": "1 times\n2 procs"
           }
