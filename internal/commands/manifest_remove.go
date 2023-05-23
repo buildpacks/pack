0631b34ead46864b4f42465bb2b24747f50e97f3
Husni Faiz <ahamedhusni73@gmail.com>
2023-05-23 19:46:06 +0530

    manfiest: add remove command
    Signed-off-by: Husni Faiz <ahamedhusni73@gmail.com>

--- internal/commands/manifest_remove.go
+++ internal/commands/manifest_remove.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"path/filepath"
+
+	"github.com/buildpacks/pack/internal/config"
 	"github.com/buildpacks/pack/internal/style"
 	"github.com/buildpacks/pack/pkg/client"
 	"github.com/buildpacks/pack/pkg/logging"
@@ -16,8 +19,19 @@ func ManifestRemove(logger logging.Logger, pack PackClient) *cobra.Command {
 		Long:    "Delete one or more manifest lists from local storage",
 		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
 			indexName := args[0]
+			manifest := args[1]
+
+			packHome, err := config.PackHome()
+			if err != nil {
+				return err
+			}
+
+			manifestDir := filepath.Join(packHome, "manifests")
+
 			if err := pack.RemoveManifest(cmd.Context(), client.RemoveManifestOptions{
-				Index: indexName,
+				Index:    indexName,
+				Path:     manifestDir,
+				Manifest: manifest,
 			}); err != nil {
 				return err
 			}
--- pkg/client/remove_manifest.go
+++ pkg/client/remove_manifest.go
@@ -1,11 +1,39 @@
 package client
 
-import "context"
+import (
+	"context"
+
+	"github.com/buildpacks/imgutil/local"
+)
 
 type RemoveManifestOptions struct {
-	Index string
+	Index    string
+	Path     string
+	Manifest string
 }
 
 func (c *Client) RemoveManifest(ctx context.Context, opts RemoveManifestOptions) error {
+	indexManifest, err := local.GetIndexManifest(opts.Index, opts.Path)
+	if err != nil {
+		return err
+	}
+
+	idx, err := local.NewIndex(opts.Index, opts.Path, local.WithManifest(indexManifest))
+	if err != nil {
+		return err
+	}
+
+	// Append manifest to local index
+	err = idx.Remove(opts.Manifest)
+	if err != nil {
+		return err
+	}
+
+	// Store index in local storage
+	err = idx.Save()
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
