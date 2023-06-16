88272d813f796ca2031832a6fd57fedcfed8a697
Domenico Luciani <dluciani@vmware.com>
2023-06-16 11:58:33 +0200

    Fixs json tests and adds tests for local and remote the rebasable images
    Signed-off-by: Domenico Luciani <dluciani@vmware.com>

--- internal/inspectimage/writer/json_test.go
+++ internal/inspectimage/writer/json_test.go
@@ -30,12 +30,79 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 		assert = h.NewAssertionManager(t)
 		outBuf bytes.Buffer
 
-		remoteInfo *client.ImageInfo
-		localInfo  *client.ImageInfo
+		remoteInfo              *client.ImageInfo
+		remoteInfoWithRebasable *client.ImageInfo
+		localInfo               *client.ImageInfo
+		localInfoWithRebasable  *client.ImageInfo
 
 		expectedLocalOutput = `{
   "local_info": {
     "stack": "test.stack.id.local",
+    "rebasable": false,
+    "base_image": {
+      "top_layer": "some-local-top-layer",
+      "reference": "some-local-run-image-reference"
+    },
+    "run_images": [
+      {
+        "name": "user-configured-mirror-for-local",
+        "user_configured": true
+      },
+      {
+        "name": "some-local-run-image"
+      },
+      {
+        "name": "some-local-mirror"
+      },
+      {
+        "name": "other-local-mirror"
+      }
+    ],
+    "buildpacks": [
+      {
+        "homepage": "https://some-homepage-one",
+        "id": "test.bp.one.local",
+        "version": "1.0.0"
+      },
+      {
+        "homepage": "https://some-homepage-two",
+        "id": "test.bp.two.local",
+        "version": "2.0.0"
+      }
+    ],
+	"extensions": null,
+    "processes": [
+      {
+        "type": "some-local-type",
+        "shell": "bash",
+        "command": "/some/local command",
+        "default": true,
+        "args": [
+          "some",
+          "local",
+          "args"
+        ],
+		"working-dir": "/some-test-work-dir"
+      },
+      {
+        "type": "other-local-type",
+        "shell": "",
+        "command": "/other/local/command",
+        "default": false,
+        "args": [
+          "other",
+          "local",
+          "args"
+        ],
+		"working-dir": "/other-test-work-dir"
+      }
+    ]
+  }
+}`
+		expectedLocalOutputWithRebasable = `{
+  "local_info": {
+    "stack": "test.stack.id.local",
+    "rebasable": true,
     "base_image": {
       "top_layer": "some-local-top-layer",
       "reference": "some-local-run-image-reference"
@@ -99,6 +166,71 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 		expectedRemoteOutput = `{  
   "remote_info": {
     "stack": "test.stack.id.remote",
+    "rebasable": false,
+    "base_image": {
+      "top_layer": "some-remote-top-layer",
+      "reference": "some-remote-run-image-reference"
+    },
+    "run_images": [
+      {
+        "name": "user-configured-mirror-for-remote",
+        "user_configured": true
+      },
+      {
+        "name": "some-remote-run-image"
+      },
+      {
+        "name": "some-remote-mirror"
+      },
+      {
+        "name": "other-remote-mirror"
+      }
+    ],
+    "buildpacks": [
+      {
+        "id": "test.bp.one.remote",
+        "version": "1.0.0",
+        "homepage": "https://some-homepage-one"
+      },
+      {
+        "id": "test.bp.two.remote",
+        "version": "2.0.0",
+        "homepage": "https://some-homepage-two"
+      }
+    ],
+	"extensions": null,
+    "processes": [
+      {
+        "type": "some-remote-type",
+        "shell": "bash",
+        "command": "/some/remote command",
+        "default": true,
+        "args": [
+          "some",
+          "remote",
+          "args"
+        ],
+		"working-dir": "/some-test-work-dir"
+      },
+      {
+        "type": "other-remote-type",
+        "shell": "",
+        "command": "/other/remote/command",
+        "default": false,
+        "args": [
+          "other",
+          "remote",
+          "args"
+        ],
+		"working-dir": "/other-test-work-dir"
+      }
+    ]
+  }
+}`
+		expectedRemoteOutputWithRebasable = `{  
+  "remote_info": {
+    "stack": "test.stack.id.remote",
+    "rebasable": true,
     "base_image": {
       "top_layer": "some-remote-top-layer",
       "reference": "some-remote-run-image-reference"
@@ -227,6 +359,62 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 				},
 			}
 
+			remoteInfoWithRebasable = &client.ImageInfo{
+				StackID: "test.stack.id.remote",
+				Buildpacks: []buildpack.GroupElement{
+					{ID: "test.bp.one.remote", Version: "1.0.0", Homepage: "https://some-homepage-one"},
+					{ID: "test.bp.two.remote", Version: "2.0.0", Homepage: "https://some-homepage-two"},
+				},
+				Base: platform.RunImageForRebase{
+					TopLayer:  "some-remote-top-layer",
+					Reference: "some-remote-run-image-reference",
+				},
+				Stack: platform.StackMetadata{
+					RunImage: platform.RunImageForExport{
+						Image:   "some-remote-run-image",
+						Mirrors: []string{"some-remote-mirror", "other-remote-mirror"},
+					},
+				},
+				BOM: []buildpack.BOMEntry{{
+					Require: buildpack.Require{
+						Name:    "name-1",
+						Version: "version-1",
+						Metadata: map[string]interface{}{
+							"RemoteData": someData{
+								String: "aString",
+								Bool:   true,
+								Int:    123,
+								Nested: struct {
+									String string
+								}{
+									String: "anotherString",
+								},
+							},
+						},
+					},
+					Buildpack: buildpack.GroupElement{ID: "test.bp.one.remote", Version: "1.0.0", Homepage: "https://some-homepage-one"},
+				}},
+				Processes: client.ProcessDetails{
+					DefaultProcess: &launch.Process{
+						Type:             "some-remote-type",
+						Command:          launch.RawCommand{Entries: []string{"/some/remote command"}},
+						Args:             []string{"some", "remote", "args"},
+						Direct:           false,
+						WorkingDirectory: "/some-test-work-dir",
+					},
+					OtherProcesses: []launch.Process{
+						{
+							Type:             "other-remote-type",
+							Command:          launch.RawCommand{Entries: []string{"/other/remote/command"}},
+							Args:             []string{"other", "remote", "args"},
+							Direct:           true,
+							WorkingDirectory: "/other-test-work-dir",
+						},
+					},
+				},
+				Rebasable: true,
+			}
+
 			localInfo = &client.ImageInfo{
 				StackID: "test.stack.id.local",
 				Buildpacks: []buildpack.GroupElement{
@@ -274,6 +462,56 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 						},
 					},
 				},
+			}
+
+			localInfoWithRebasable = &client.ImageInfo{
+				StackID: "test.stack.id.local",
+				Buildpacks: []buildpack.GroupElement{
+					{ID: "test.bp.one.local", Version: "1.0.0", Homepage: "https://some-homepage-one"},
+					{ID: "test.bp.two.local", Version: "2.0.0", Homepage: "https://some-homepage-two"},
+				},
+				Base: platform.RunImageForRebase{
+					TopLayer:  "some-local-top-layer",
+					Reference: "some-local-run-image-reference",
+				},
+				Stack: platform.StackMetadata{
+					RunImage: platform.RunImageForExport{
+						Image:   "some-local-run-image",
+						Mirrors: []string{"some-local-mirror", "other-local-mirror"},
+					},
+				},
+				BOM: []buildpack.BOMEntry{{
+					Require: buildpack.Require{
+						Name:    "name-1",
+						Version: "version-1",
+						Metadata: map[string]interface{}{
+							"LocalData": someData{
+								Bool: false,
+								Int:  456,
+							},
+						},
+					},
+					Buildpack: buildpack.GroupElement{ID: "test.bp.one.remote", Version: "1.0.0", Homepage: "https://some-homepage-one"},
+				}},
+				Processes: client.ProcessDetails{
+					DefaultProcess: &launch.Process{
+						Type:             "some-local-type",
+						Command:          launch.RawCommand{Entries: []string{"/some/local command"}},
+						Args:             []string{"some", "local", "args"},
+						Direct:           false,
+						WorkingDirectory: "/some-test-work-dir",
+					},
+					OtherProcesses: []launch.Process{
+						{
+							Type:             "other-local-type",
+							Command:          launch.RawCommand{Entries: []string{"/other/local/command"}},
+							Args:             []string{"other", "local", "args"},
+							Direct:           true,
+							WorkingDirectory: "/other-test-work-dir",
+						},
+					},
+				},
+				Rebasable: true,
 			}
 
 			outBuf = bytes.Buffer{}
@@ -343,6 +581,35 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 				assert.NotContains(outBuf.String(), "test.stack.id.remote")
 				assert.ContainsJSON(outBuf.String(), expectedLocalOutput)
 			})
+			it("prints local rebasable image info in JSON format", func() {
+				runImageMirrors := []config.RunImage{
+					{
+						Image:   "un-used-run-image",
+						Mirrors: []string{"un-used"},
+					},
+					{
+						Image:   "some-local-run-image",
+						Mirrors: []string{"user-configured-mirror-for-local"},
+					},
+					{
+						Image:   "some-remote-run-image",
+						Mirrors: []string{"user-configured-mirror-for-remote"},
+					},
+				}
+				sharedImageInfo := inspectimage.GeneralInfo{
+					Name:            "test-image",
+					RunImageMirrors: runImageMirrors,
+				}
+				jsonWriter := writer.NewJSON()
+
+				logger := logging.NewLogWithWriters(&outBuf, &outBuf)
+				err := jsonWriter.Print(logger, sharedImageInfo, localInfoWithRebasable, nil, nil, nil)
+				assert.Nil(err)
+
+				assert.ContainsJSON(outBuf.String(), `{ "image_name": "test-image" }`)
+				assert.ContainsJSON(outBuf.String(), expectedLocalOutputWithRebasable)
+				assert.NotContains(outBuf.String(), "test.stack.id.remote")
+			})
 		})
 
 		when("only remote image exists", func() {
@@ -374,6 +641,35 @@ func testJSON(t *testing.T, when spec.G, it spec.S) {
 				assert.ContainsJSON(outBuf.String(), `{ "image_name": "test-image" }`)
 				assert.NotContains(outBuf.String(), "test.stack.id.local")
 				assert.ContainsJSON(outBuf.String(), expectedRemoteOutput)
+			})
+			it("prints remote rebasable image info in JSON format", func() {
+				runImageMirrors := []config.RunImage{
+					{
+						Image:   "un-used-run-image",
+						Mirrors: []string{"un-used"},
+					},
+					{
+						Image:   "some-local-run-image",
+						Mirrors: []string{"user-configured-mirror-for-local"},
+					},
+					{
+						Image:   "some-remote-run-image",
+						Mirrors: []string{"user-configured-mirror-for-remote"},
+					},
+				}
+				sharedImageInfo := inspectimage.GeneralInfo{
+					Name:            "test-image",
+					RunImageMirrors: runImageMirrors,
+				}
+				jsonWriter := writer.NewJSON()
+
+				logger := logging.NewLogWithWriters(&outBuf, &outBuf)
+				err := jsonWriter.Print(logger, sharedImageInfo, nil, remoteInfoWithRebasable, nil, nil)
+				assert.Nil(err)
+
+				assert.ContainsJSON(outBuf.String(), `{ "image_name": "test-image" }`)
+				assert.NotContains(outBuf.String(), "test.stack.id.local")
+				assert.ContainsJSON(outBuf.String(), expectedRemoteOutputWithRebasable)
 			})
 		})
 	})
