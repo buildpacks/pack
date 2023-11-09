07b9150f061a46326c831ef290f96dcd17b47606
WYGIN <wygininc@gmail.com>
2023-11-09 03:07:47 +0000

    WIP added manifest_remove command
    Signed-off-by: WYGIN <wygininc@gmail.com>

---
+++ internal/commands/manifest_remove.go
@@ -0,0 +1,34 @@
+package commands
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/buildpacks/pack/pkg/logging"
+)
+
+// ManifestDeleteFlags define flags provided to the ManifestDelete
+// type ManifestDeleteFlags struct {
+// }
+
+// ManifestDelete deletes one or more manifest lists from local storage
+func ManifestDelete(logger logging.Logger, pack PackClient) *cobra.Command {
+	// var flags ManifestDeleteFlags
+
+	cmd := &cobra.Command{
+		Use:     "pack manifest remove [manifest-list] [manifest-list...] [flags]",
+		Args:    cobra.MatchAll(cobra.MinimumNArgs(1), cobra.OnlyValidArgs),
+		Short:   "Delete one or more manifest lists from local storage",
+		Example: `pack manifest remove cnbs/sample-package:hello-multiarch-universe`,
+		Long: `Delete one or more manifest lists from local storage.
+		When a manifest list exits locally, users can remove existing images from a manifest list`,
+		RunE: logError(logger, func(cmd *cobra.Command, args []string) error {
+			if err := pack.DeleteManifest(cmd.Context(), args); err != nil {
+				return err
+			}
+			return nil
+		}),
+	}
+
+	AddHelpFlag(cmd, "remove")
+	return cmd
+}
---
+++ internal/commands/manifest_remove_test.go
@@ -0,0 +1,61 @@
+package commands_test
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/buildpacks/pack/internal/commands"
+	"github.com/buildpacks/pack/internal/commands/testmocks"
+	"github.com/buildpacks/pack/pkg/logging"
+	"github.com/golang/mock/gomock"
+	"github.com/heroku/color"
+	"github.com/sclevine/spec"
+	"github.com/sclevine/spec/report"
+	"github.com/spf13/cobra"
+)
+
+func TestManifestDeleteCommand(t *testing.T) {
+	color.Disable(true)
+	defer color.Disable(false)
+
+	spec.Run(t, "Commands", testManifestDeleteCommand, spec.Random(), spec.Report(report.Terminal{}))
+}
+
+func testManifestDeleteCommand(t *testing.T, when spec.G, it spec.S) {
+	var (
+		command        *cobra.Command
+		logger         *logging.LogWithWriters
+		outBuf         bytes.Buffer
+		mockController *gomock.Controller
+		mockClient     *testmocks.MockPackClient
+	)
+
+	it.Before(func() {
+		logger = logging.NewLogWithWriters(&outBuf, &outBuf)
+		mockController = gomock.NewController(t)
+		mockClient = testmocks.NewMockPackClient(mockController)
+
+		command = commands.ManifestDelete(logger, mockClient)
+	})
+
+	when("#ManifestRemove", func() {
+		when("when no args passed", func() {
+
+		})
+		when("when manifest list reference is incorrect", func() {
+
+		})
+		when("when manifest passed in-place of manifest list on first arg", func() {
+
+		})
+		when("when manifest list is locally available", func() {
+
+		})
+		when("when manifest list is not locally available", func() {
+
+		})
+		when("when multiple manifests passed", func() {
+
+		})
+	})
+}
