9985d9398de2e67f46f8a4da7c384d0916f2b77f
Juan Bustamante <bustamantejj@gmail.com>
2024-02-07 16:31:45 -0500

    running make format
    Signed-off-by: Juan Bustamante <bustamantejj@gmail.com>
    Signed-off-by: Pratham Agarwal <agarwalpratham1812@gmail.com>

--- acceptance/invoke/pack.go
+++ acceptance/invoke/pack.go
@@ -269,7 +269,7 @@ var featureTests = map[Feature]func(i *PackInvoker) bool{
 	},
 	UnsupportedSchemaKeysWarning: func(i *PackInvoker) bool {
 		return i.atLeast("v0.33.0")
-  	},
+	},
 	FlattenBuilderCreationV2: func(i *PackInvoker) bool {
 		return i.atLeast("v0.33.1")
 	},
