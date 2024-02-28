6b12f382ded35ca65aa0a6bb51c8bc7111d7cb07
github-action-benchmark <github@users.noreply.github.com>
2024-02-28 20:25:04 +0000

    add Go Benchmark (go) benchmark result for 3356686e25870ccd513f64e5fbdbb9083654f234

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1708519886958,
+  "lastUpdate": 1709151904162,
   "repoUrl": "https://github.com/buildpacks/pack",
   "entries": {
     "Go Benchmark": [
@@ -5362,6 +5362,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkBuild/with_Additional_Buildpack",
             "value": 56822356963,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "juan.bustamante@broadcom.com",
+            "name": "Juan Bustamante",
+            "username": "jjbustamante"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "3356686e25870ccd513f64e5fbdbb9083654f234",
+          "message": "Merge pull request #2082 from buildpacks/dependabot/go_modules/go-dependencies-0ae3d4a4b9\n\nbuild(deps): bump the go-dependencies group with 2 updates",
+          "timestamp": "2024-02-28T15:22:37-05:00",
+          "tree_id": "3686ac315062b56b27fe9ac3194988a0e4b9b195",
+          "url": "https://github.com/buildpacks/pack/commit/3356686e25870ccd513f64e5fbdbb9083654f234"
+        },
+        "date": 1709151903050,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 7199262616,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 2652998827,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Additional_Buildpack",
+            "value": 59692574422,
             "unit": "ns/op",
             "extra": "1 times\n4 procs"
           }
