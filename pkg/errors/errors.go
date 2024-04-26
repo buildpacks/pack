e6fb2b740ad619c58b6c4a513ca418597866dcfc
Juan Bustamante <jbustamante@vmware.com>
2024-04-26 16:57:14 -0500

    WIP - removing some unused files
    Signed-off-by: Juan Bustamante <jbustamante@vmware.com>

--- pkg/errors/errors.go
+++
@@ -1,9 +0,0 @@
-package errors
-
-import "errors"
-
-var (
-	ErrDuplicateName      = errors.New("Image/ImageIndex with the given name exists")
-	ErrIndexUnknown       = errors.New("cannot find Image Index with the given name")
-	ErrNotAddManifestList = errors.New("error while adding ImageIndex to the list")
-)
