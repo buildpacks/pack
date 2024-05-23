9fb24ec704cdab375a71ac8977728e5baed29f5d
github-action-benchmark <github@users.noreply.github.com>
2024-05-23 14:16:10 +0000

    add Go Benchmark (go) benchmark result for ebea3ae82e9ff650dc2da482332bd14ad99759e1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1716407290399,
+  "lastUpdate": 1716473770593,
   "repoUrl": "https://github.com/buildpacks/pack",
   "entries": {
     "Go Benchmark": [
@@ -6832,6 +6832,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkBuild/with_Additional_Buildpack",
             "value": 54187426311,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "juan.bustamante@broadcom.com",
+            "name": "Juan Bustamante",
+            "username": "jjbustamante"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ebea3ae82e9ff650dc2da482332bd14ad99759e1",
+          "message": "Merge pull request #2162 from buildpacks/build-with-platform\n\nSupport `pack build --platform`",
+          "timestamp": "2024-05-23T09:14:06-05:00",
+          "tree_id": "709d47776f58fa9bef3510d82d1f87add7442480",
+          "url": "https://github.com/buildpacks/pack/commit/ebea3ae82e9ff650dc2da482332bd14ad99759e1"
+        },
+        "date": 1716473769332,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 7126769852,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 2991523315,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Additional_Buildpack",
+            "value": 64693754939,
             "unit": "ns/op",
             "extra": "1 times\n4 procs"
           }
