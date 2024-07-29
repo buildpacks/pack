293ed9afa69e2f721277a1da59293fb6837323a2
github-action-benchmark <github@users.noreply.github.com>
2024-07-29 22:08:44 +0000

    add Go Benchmark (go) benchmark result for a651c5dac4c2f5d7232b164634d4aa904415cd94

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1722279534346,
+  "lastUpdate": 1722290924316,
   "repoUrl": "https://github.com/buildpacks/pack",
   "entries": {
     "Go Benchmark": [
@@ -7588,6 +7588,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "BenchmarkBuild/with_Additional_Buildpack",
             "value": 55940879825,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "narellano@vmware.com",
+            "name": "Natalie Arellano",
+            "username": "natalieparellano"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "a651c5dac4c2f5d7232b164634d4aa904415cd94",
+          "message": "Merge pull request #2234 from sap-contributions/support-oci-mediatype\n\nSupport OCI layouts with `application/vnd.oci.image.manifest.v1+json` media type",
+          "timestamp": "2024-07-29T18:07:08-04:00",
+          "tree_id": "2dc359bd23bec4f7aa67a154f2ef8069e7186c9e",
+          "url": "https://github.com/buildpacks/pack/commit/a651c5dac4c2f5d7232b164634d4aa904415cd94"
+        },
+        "date": 1722290923751,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkBuild/with_Untrusted_Builder",
+            "value": 4827650019,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Trusted_Builder",
+            "value": 1535446181,
+            "unit": "ns/op",
+            "extra": "1 times\n4 procs"
+          },
+          {
+            "name": "BenchmarkBuild/with_Additional_Buildpack",
+            "value": 57425554814,
             "unit": "ns/op",
             "extra": "1 times\n4 procs"
           }
