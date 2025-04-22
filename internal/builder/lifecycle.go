287b74f99724f73c4c1d31af1f8c5221f0f71843
Juan Bustamante <bustamantejj@gmail.com>
2025-04-22 18:13:08 -0500

    Updating to lifecyle 0.20.8
    Signed-off-by: Juan Bustamante <bustamantejj@gmail.com>

--- internal/builder/lifecycle.go
+++ internal/builder/lifecycle.go
@@ -14,7 +14,7 @@ import (
 
 // DefaultLifecycleVersion A snapshot of the latest tested lifecycle version values
 const (
-	DefaultLifecycleVersion = "0.20.6"
+	DefaultLifecycleVersion = "0.20.8"
 )
 
 // Blob is an interface to wrap opening blobs
